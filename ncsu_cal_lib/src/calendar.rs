91eecc548dc261fcea284cdf969c05fa4abffa26
Bennett Petzold <bennettpetzold01@gmail.com>
2023-11-20 10:23:15 -0500

    Add clippy lints

--- ncsu_cal_lib/src/calendar.rs
+++ ncsu_cal_lib/src/calendar.rs
@@ -7,7 +7,7 @@
 *
 * ncsu_exam_calendar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License along with ncsu_exam_calendar. If not, see <https://www.gnu.org/licenses/>. 
+* You should have received a copy of the GNU General Public License along with ncsu_exam_calendar. If not, see <https://www.gnu.org/licenses/>.
 */
 
 use std::{
@@ -17,7 +17,7 @@ use std::{
 };
 
 use anyhow::{anyhow, bail, Result};
-use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, Utc};
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
